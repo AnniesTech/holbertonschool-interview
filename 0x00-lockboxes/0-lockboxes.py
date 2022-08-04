dc94f4f6f86b0f0684e81e796708f04a8c4c2be1
AnniesTech <anita.rocha0505@gmail.com>
2022-08-04 13:56:06 -0500

    task 0 corrected

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Method that determines if all the boxes can be opened. """
+
+
 def canUnlockAll(boxes):
+    """ Function that determine if all the boxes can be opened. """
     newlist = [0]
     for i in newlist:
         for box in boxes[i]:
